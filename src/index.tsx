981f31cb56b8ae5daf081b87c65edc6d041dc7d6
ChanNaru <narutyan0711@gmail.com>
2022-08-09 13:54:40 +0900

    upload modules

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const domContent = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(domContent)
+root.render(
+  <React.StrictMode>
+    <h1>aaa</h1>
+  </React.StrictMode>
+)
