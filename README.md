7f6dab0bf8475fc7b81865138251950985f9d352
ChanNaru <narutyan0711@gmail.com>
2022-08-09 14:50:56 +0900

    convert some html elements to jsx elements

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Site's HTML entities are in `docs/`.
 
 ## Project details
 
-These project files can be bundled by [Parcel](https://parceljs.org/).
+These project files can be bundled by [Parcel](https://parceljs.org/).\
 And some npm scripts can activate it (please look at `package.json` for more details).
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,16 @@ const domContent = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(domContent)
 root.render(
   <React.StrictMode>
-    <h1>aaa</h1>
+    <h1>Contents to write</h1>
+    <ul>
+        <li>name</li>
+        <li>image</li>
+        <li>mail</li>
+        <li>career</li>
+        <li>technical stuck</li>
+        <li>github</li>
+        <li>hatena</li>
+        <li>qiita</li>
+    </ul>
   </React.StrictMode>
 )
