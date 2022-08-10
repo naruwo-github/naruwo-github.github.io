298ec156ec836ffa96703561841a9ad5d10ea7f9
ChanNaru <narutyan0711@gmail.com>
2022-08-10 10:08:08 +0900

    update mv explanation

--- src/components/mv.tsx
+++ src/components/mv.tsx
@@ -12,6 +12,14 @@ const Mv = () => {
         <p className="mv-text">
           <a href="#skill">SKILL</a>に関連するコーディングはおまかせください。
         </p>
+        <br/>
+        <br/>
+        <p className="mv-text">
+          <a href="https://github.com/naruwo-github" target="_blank" rel="noopener">Github</a>
+          , <a href="https://chan-naru.hatenablog.com/" target="_blank" rel="noopener">はてなブログ</a>
+          , <a href="https://qiita.com/chan_naruwo" target="_blank" rel="noopener">Qiita</a>
+          にて情報発信しています。
+        </p>
       </div>
     </div>
   )
