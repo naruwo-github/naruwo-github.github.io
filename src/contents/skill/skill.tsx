a9201f0c5ec97606ec3893ed6bcf39e6f85a3f86
ChanNaru <narutyan0711@gmail.com>
2022-08-20 22:09:12 +0900

    supported switching on skill

--- src/contents/skill/skill.tsx
+++ src/contents/skill/skill.tsx
@@ -1,26 +1,35 @@
 import './style.css'
 import skillItems from "./skillItems"
+import { useContext } from 'react'
+import { LanguageContext } from '../../app'
 
 const Skill = () => {
+  const {isSwitched, setIsSwitched} = useContext(LanguageContext)
+
   return (
-          <section className="skill section" id="skill">
-        <div className="container">
-          <h2 className="title">SKILL</h2>
-          <div className="skill-list">
-            {skillItems.map((item, index) => (
-            <div key={index} className="skill-item">
-              <p className="skill-img"><img src={item.img} alt=""/></p>
-              <div className="skill-body">
-                <h3 className="skill-name">{item.name}</h3>
-                {item.text.map((sentence, index) => (
+    <section className="skill section" id="skill">
+      <div className="container">
+        <h2 className="title">SKILL</h2>
+        <div className="skill-list">
+          {skillItems.map((item, index) => (
+          <div key={index} className="skill-item">
+            <p className="skill-img"><img src={item.img} alt=""/></p>
+            <div className="skill-body">
+              <h3 className="skill-name">{item.name}</h3>
+              {isSwitched
+              ? item.textEn.map((sentence, index) => (
                 <p key={index} className="skill-text">{sentence}</p>
-                ))}
-              </div>
+                ))
+              : item.textJp.map((sentence, index) => (
+                <p key={index} className="skill-text">{sentence}</p>
+                ))
+              }
             </div>
-            ))}
           </div>
+          ))}
         </div>
-      </section>
+      </div>
+    </section>
   )
 }
 
