05ccca642a589124ef7a51006317c1e03e6ab36e
ChanNaru <narutyan0711@gmail.com>
2022-08-20 20:08:04 +0900

    supported switching at contact

--- src/contents/contact/contact.tsx
+++ src/contents/contact/contact.tsx
@@ -1,12 +1,19 @@
+import { useContext } from 'react'
+import { LanguageContext } from '../../app'
 import './style.css'
 
 const Contact = () => {
+  const {isSwitched, setIsSwitched} = useContext(LanguageContext)
+
   return (
     <section className="contact section" id="contact">
       <div className="container">
         <h2 className="title">CONTACT</h2>
         <p className="lead">
-          お問い合わせは、SNSかメールにてお願いいたします。
+          {isSwitched
+          ? 'Please contact me via SNS or email.'
+          : 'お問い合わせは、SNSかメールにてお願いいたします。'
+          }
         </p>
         <div className="contact-list">
           <a className="contact-item" href="https://www.instagram.com/chan_naru_way/" target="_blank" rel="noopener">Instagram</a>
