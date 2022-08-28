afa33c850b6c328c61ef52c13e1f1f081daae603
ChanNaru <narutyan0711@gmail.com>
2022-08-28 23:42:59 +0900

    add works

--- src/contents/works/worksItems.ts
+++ src/contents/works/worksItems.ts
@@ -1,15 +1,36 @@
 import signboardPreparing from '../../assets/signboard_preparing.png'
+import doubleRoulette from '../../assets/double-roulette.png'
+import warican from '../../assets/warican.png'
+import ams from '../../assets/asset-management-simulator.png'
 import { URL_GITHUB, URL_HATENA, URL_QIITA } from '../../config/env'
 
 const worksItems = [
   {
-    ref: URL_GITHUB,
-    img: signboardPreparing,
-    name: 'name of the work',
-    info: 'Design / Coding(Responsive)'
+    ref: '/ams/index.html',
+    img: ams,
+    name: 'Asset Management Simulator',
+    info: 'Instant simulator of asset management for web.'
+  },
+  {
+    ref: `${URL_GITHUB}/DoubleRoulette/blob/master/README.md`,
+    img: doubleRoulette,
+    name: 'Double Roulette - decide two at the same time!',
+    info: 'Roulette app for iOS.'
+  },
+  {
+    ref: `${URL_GITHUB}/AndroidDoubleRoulette/blob/main/README.md`,
+    img: doubleRoulette,
+    name: 'Double Roulette - decide two at the same time!',
+    info: 'Roulette app for Android.'
   },
   {
     ref: URL_HATENA,
+    img: warican,
+    name: 'WariCan¥',
+    info: 'Splitting bill app for iOS.'
+  },
+  {
+    ref: URL_QIITA,
     img: signboardPreparing,
     name: 'preparing...',
     info: 'preparing...'
@@ -20,12 +41,6 @@ const worksItems = [
     name: '...',
     info: '...'
   },
-  {
-    ref: 'works.html',
-    img: '',
-    name: '',
-    info: ''
-  }
 ]
 
 export default worksItems
