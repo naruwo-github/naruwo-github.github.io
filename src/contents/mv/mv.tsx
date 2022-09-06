e2a4f76bbc5c4a1af0d897e8188052d6ca62b155
ChanNaru <narutyan0711@gmail.com>
2022-09-06 09:56:00 +0900

    add gif on mv

--- src/contents/mv/mv.tsx
+++ src/contents/mv/mv.tsx
@@ -4,17 +4,18 @@ import { useContext } from 'react'
 import { LanguageContext } from '../../app'
 
 const Mv = () => {
-  const {isSwitched, setIsSwitched} = useContext(LanguageContext)
+  const { isSwitched, setIsSwitched } = useContext(LanguageContext)
 
   return (
     <div className="mv">
+      <div className='mv-background-with-image' />
       <div className="mv-container">
         <p className="mv-title">{AUTHOR}</p>
         <p className="mv-subtitle">{TITLE}</p>
         <p className="mv-text">
           {isSwitched
-          ? 'I make you satisfied with by creating some applications/softwares.'
-          : 'ユーザーとお客様が心から満足できるアプリケーション/ソフトウェアをお作りいたします。'
+            ? 'I make you satisfied with by creating some applications/softwares.'
+            : 'ユーザーとお客様が心から満足できるアプリケーション/ソフトウェアをお作りいたします。'
           }
         </p>
         <p className="mv-text">
@@ -22,15 +23,15 @@ const Mv = () => {
           <a href="#skill">SKILL</a>
           {!isSwitched && 'に関連するコーディングはおまかせください。'}
         </p>
-        <br/>
-        <br/>
+        <br />
+        <br />
         <p className="mv-text">
           <a href={URL_GITHUB} target="_blank" rel="noopener">Github</a>
           , <a href={URL_HATENA} target="_blank" rel="noopener">はてなブログ</a>
           , <a href={URL_QIITA} target="_blank" rel="noopener">Qiita</a>
           {isSwitched
-          ? ' are also my portfolio, so please take a look :)'
-          : 'にて情報発信しています。'
+            ? ' are also my portfolio, so please take a look :)'
+            : 'にて情報発信しています。'
           }
         </p>
       </div>
